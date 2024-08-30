79c1a6fc75db558c99b442e577a3d796cfd019bb
JASantacruz00 <joseantonio.santacruz00@gmail.com>
2024-08-31 00:46:58 +0200

    Fix styles error centering description text

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-flow-col grid-cols-12 h-[90vh] w-screen">
     <div class="col-span-6 flex flex-col justify-center pl-64 mb-36">
       <h1 class="text-center font-bold text-8xl mb-12">Password Generator.</h1>
-      <h2 class="font-semibold text-xl">
+      <h2 class="w-full text-center font-semibold text-xl px-28 align-middle">
         Create strong, secure passwords with ease. Customize your password
         settings and generate unbreakable codes in seconds.
       </h2>
