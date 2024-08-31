e7902978ae3c182ba663b509fc2eeaf11dbef617
JASantacruz00 <jsantacgdev@gmail.com>
2024-08-31 14:19:38 +0200

    Update margins

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="grid grid-flow-col grid-cols-12 h-[90vh] w-screen">
-    <div class="col-span-6 flex flex-col justify-center pl-64 mb-24">
+    <div class="col-span-6 flex flex-col mt-36 pl-64 mb-24">
       <h1 class="text-center font-bold text-8xl mb-12">Password Generator.</h1>
       <h2 class="w-full text-center font-semibold text-xl px-28 align-middle">
         Create strong, secure passwords with ease. Customize your password
         settings and generate unbreakable codes in seconds.
       </h2>
     </div>
-    <div class="col-span-6 flex flex-col justify-center pr-64">
+    <div class="col-span-6 flex flex-col mt-36 pr-64">
       <PasswordGenerator />
     </div>
   </div>
